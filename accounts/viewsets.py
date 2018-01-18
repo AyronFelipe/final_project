467da054438521e0540f014c72bd086a96d1c205
Ayron Felipe Carvalho Araujo <ayron41@gmail.com>
2018-01-18 12:01:29 -0300

    Trocando status de criação
    Signed-off-by: Ayron Felipe Carvalho Araujo <ayron41@gmail.com>

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -66,7 +66,7 @@ class CreatePersonViewSet(generics.CreateAPIView):
             person = Person(email=instance.get('email'), is_active=instance.get('is_active'), first_name=instance.get('first_name'), last_name=instance.get('last_name'), cpf=instance.get('cpf'), birthday=instance.get('birthday'), phone=instance.get('phone'), cell_phone=instance.get('cell_phone'), neighborhood=instance.get('neighborhood'), street=instance.get('street'), number=instance.get('number'))
             person.set_password(instance.get('password'))
             person.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -86,5 +86,5 @@ class CreateInstitutionViewSet(generics.CreateAPIView):
             institution = Institution(email=instance.get('email'), is_active=instance.get('is_active'), name=instance.get('name'), cnpj=instance.get('cnpj'), phone=instance.get('phone'), cell_phone=instance.get('cell_phone'), neighborhood=instance.get('neighborhood'), street=instance.get('street'), number=instance.get('number'))
             institution.set_password(instance.get('password'))
             institution.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
