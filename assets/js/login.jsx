d11ad109f11f567b4dc2b97d5f3183b99b8e5bd3
Ayron Felipe Carvalho Araujo <ayron41@gmail.com>
2018-01-25 17:37:56 -0300

    Layout login finalizado
    Signed-off-by: Ayron Felipe Carvalho Araujo <ayron41@gmail.com>

--- assets/js/login.jsx
+++ assets/js/login.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Footer from './footer'
 
 export default class Login extends React.Component{
 
     render(){
         return(
-            <div className="valign-wrapper row">
-                <div className="col card hoverable s10 pull-s1 m6 pull-m3 l4 pull-l4">
-                    <form>
-                        <div className="card-content">
-                            <span className="card-title">Enter credentials</span>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <label htmlFor="email">Email address</label>
-                                    <input type="email" className="validate" name="email" id="email" />
-                                </div>
-                                <div className="input-field col s12">
-                                    <label htmlFor="password">Password </label>
-                                    <input type="password" className="validate" name="password" id="password" />
+            <div className="deep-purple darken-2">
+                <div className="valign-wrapper row">
+                    <div className="col card hoverable s10 pull-s1 m6 pull-m3 l4 pull-l4 deep-purple white-text">
+                        <form>
+                            <div className="card-content">
+                                <div className="white-text center-align"><h1>Nome do Projeto</h1></div>
+                                <span className="card-title">Digite seus dados</span>
+                                <div className="row">
+                                    <div className="input-field col s12">
+                                        <label htmlFor="email" className="white-text">Email</label>
+                                        <input type="email" className="validate" name="email" id="email" />
+                                    </div>
+                                    <div className="input-field col s12">
+                                        <label htmlFor="password" className="white-text">Senha</label>
+                                        <input type="password" className="validate" name="password" id="password" />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="card-action right-align">
-                            <input type="reset" id="reset" className="btn-flat grey-text waves-effect" />
-                            <input type="submit" className="btn green waves-effect waves-light" value="Login" />
-                        </div>
-                    </form>
+                            <div className="card-action right-align">
+                                <button className="btn waves-effect waves-light indigo accent-2 white-text">Entrar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+                <Footer />
             </div>
         )
     }
