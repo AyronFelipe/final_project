97a6e909f65124a7a5740016ec93bbe587ba8785
Ayron Felipe Carvalho Araujo <ayron41@gmail.com>
2018-06-08 15:55:53 -0300

    Signed-off-by: Ayron Felipe Carvalho Araujo <ayron41@gmail.com>

---
+++ project_settings/production.py
@@ -0,0 +1,13 @@
+import os
+import django_heroku
+from decouple import config, Csv
+import dj_database_url.config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
--- project_settings/wsgi.py
+++ project_settings/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_settings.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
