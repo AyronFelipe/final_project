37d833229044579a067ea39a58282fe400ca01ca
Ayron Felipe Carvalho Araujo <ayron41@gmail.com>
2018-06-18 15:57:47 -0300

    Implementação detalhe da doação
    Signed-off-by: Ayron Felipe Carvalho Araujo <ayron41@gmail.com>

--- assets/js/donationdetail.jsx
+++ assets/js/donationdetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { storageToken } from './auth'
+import * as Vibrant from 'node-vibrant'
 
 export default class DonationDetail extends React.Component{
 
@@ -26,11 +27,17 @@ export default class DonationDetail extends React.Component{
                 console.log(request, status, err);
             }
         });
+
+
     }
 
     render(){
+        if( this.state.donation.photo!=undefined ){
+            let color = new Vibrant(this.state.donation.photo);
+            color.getPalette((err, palette) => $("#content").css("background-color", palette.Vibrant.getHex()));
+        }
         return(
-            <div>
+            <div id="content">
                 <nav className="nav-extended deep-purple darken-2 white-text">
                     <div className="row">
                         <div className="col s12">
@@ -47,6 +54,30 @@ export default class DonationDetail extends React.Component{
                         </div>
                     </div>
                 </nav>
+                <br/><br/>
+                <div className="row">
+                    <div className="col s12">
+                        <div className="col s10 push-s1">
+                            <div className="card">
+                                <div className="card-content">
+                                    <div className="row">
+                                        <div className="col l5 m12">
+                                            <img className="responsive-img" src={ this.state.donation.photo } />
+                                            <button className="btn-large waves-effect waves-light indigo accent-2 white-text" style={{width: '100%'}}>Solicitar esta doação</button>
+                                        </div>
+                                        <div className="col l7 m12 purple-text">
+                                            <h3>{ this.state.donation.name }</h3>
+                                            <br/><br/>
+                                            <p>
+                                                <span>{ this.state.donation.description }</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
