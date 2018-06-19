808652e267e8693ef4282d0ae83769f278c7ee48
Ayron Felipe Carvalho Araujo <ayron41@gmail.com>
2018-06-19 13:49:51 -0300

    Signed-off-by: Ayron Felipe Carvalho Araujo <ayron41@gmail.com>

--- assets/js/donationdetail.jsx
+++ assets/js/donationdetail.jsx
@@ -27,11 +27,12 @@ export default class DonationDetail extends React.Component{
                 console.log(request, status, err);
             }
         });
-
-
     }
 
     render(){
+        console.log(this.state.donation)
+        const API_KEY = "AIzaSyCq-XgDdK7Ewn_BWMxXpiDVn04y_BHB4yY"
+
         if( this.state.donation.photo!=undefined ){
             let color = new Vibrant(this.state.donation.photo);
             color.getPalette((err, palette) => $("#content").css("background-color", palette.Vibrant.getHex()));
@@ -60,17 +61,30 @@ export default class DonationDetail extends React.Component{
                         <div className="col s10 push-s1">
                             <div className="card">
                                 <div className="card-content">
-                                    <div className="row">
-                                        <div className="col l5 m12">
+                                    <div className="row purple-text">
+                                        <div className="col l5 m12 s12">
                                             <img className="responsive-img" src={ this.state.donation.photo } />
-                                            <button className="btn-large waves-effect waves-light indigo accent-2 white-text" style={{width: '100%'}}>Solicitar esta doação</button>
-                                        </div>
-                                        <div className="col l7 m12 purple-text">
-                                            <h3>{ this.state.donation.name }</h3>
                                             <br/><br/>
+                                            <span>Doação realizada por: <div className="chip">{ this.state.donation.donator }</div></span>
+                                            <button className="btn-large waves-effect waves-light indigo accent-2 white-text" style={{width: '100%'}}>Solicitar esta doação</button>
+                                            <h4>Ponto de Encontro</h4>
+                                            <div className="video-container">
+                                                <iframe width="450" height="350" frameBorder="0" style={{border:0}} src={`https://www.google.com/maps/embed/v1/place?q=${this.state.donation.cep},${this.state.donation.neighborhood},${this.state.donation.street},${this.state.donation.number},+Brasil&key=${API_KEY}`} allowFullScreen></iframe>
+                                            </div>
+                                        </div>
+                                        <div className="col l7 m12 s12">
+                                            <h3>{ this.state.donation.name }</h3>
+                                            <br/>
                                             <p>
                                                 <span>{ this.state.donation.description }</span>
                                             </p>
+                                            <br/>
+                                            <h4>Histórico da Doação</h4>
+                                        </div>
+                                    </div>
+                                    <div className="row purple-text">
+                                        <div className="center-align">
+                                            <h4>Galeria de Imagens da Doação</h4>
                                         </div>
                                     </div>
                                 </div>
--- donations/serializers.py
+++ donations/serializers.py
@@ -4,6 +4,8 @@ from .models import Donation
 
 class DonationSerializer(serializers.ModelSerializer):
 
+    donator = serializers.StringRelatedField()
+
     class Meta:
         model = Donation
-        fields = ['name', 'description', 'validity', 'validity_hour', 'photo', 'neighborhood', 'street', 'number', 'cep', 'uf', 'city', 'complement', 'pk', 'slug']
+        fields = ['donator', 'name', 'description', 'validity', 'validity_hour', 'photo', 'neighborhood', 'street', 'number', 'cep', 'uf', 'city', 'complement', 'pk', 'slug']
